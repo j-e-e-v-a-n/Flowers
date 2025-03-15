ba8c396014aed772aa73de9d487b04e9de950f00
Jeevan <103121525+j-e-e-v-a-n@users.noreply.github.com>
2025-03-15 18:48:48 +0530

    ,

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+  // Remove the 'not-loaded' class when the page is fully loaded
+window.addEventListener('load', () => {
+    document.body.classList.remove('not-loaded');
+});
+
+// Smooth scroll to gallery after delay
+setTimeout(() => {
+    document.getElementById('gallery').scrollIntoView({ 
+        behavior: 'smooth'
+    });
+}, 14000); // Scroll after animations complete
+
+document.addEventListener("DOMContentLoaded", function () {
+  const closeGalleryBtn = document.getElementById("closeGalleryBtn");
+  const galleryContainer = document.getElementById("gallery");
+
+  closeGalleryBtn.addEventListener("click", function () {
+    galleryContainer.style.display = "none";
+  });
+});
